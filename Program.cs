03419fde2303861834a0c1c7b115c2eabbf947f3
andifilhohub <andersonfilho.contatar@outlook.com>
2024-12-16 15:52:18 -0300

    first class

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r1 = new Retangulo(); // criando um objeto r1 da classe Retangulo
+            Retangulo r2 = new Retangulo(); // criando um objeto r2 da classe Retangulo
+
+
+            r1.imprimeDados();
+            r2.imprimeDados();
+
+            r1.setComprimento(10.5);
+            r1.setAltura(8.8);
+
+            r1.imprimeDados();
+            r2.imprimeDados();
+
+            Console.WriteLine("perimentro = " + r1.Perimetro());
+            Console.WriteLine("Area = " + r2.Area());
+
+            Retangulo r3 = new Retangulo(5.8, 12.5);
+
+            Console.WriteLine("perimentro do r3 = " + r3.Perimetro());
+            Console.WriteLine("Area do r3 = " + r3.Area());
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caRetangulo
+{
+    internal class Retangulo
+    {
+        //Atributos
+        private double comprimento;
+        private double altura;
+
+        //métodos
+
+        public Retangulo()
+        {
+            comprimento= 5.0;
+            altura= 5.0; 
+        }
+
+        public Retangulo(double comp, double alt)
+        {
+            comprimento = comp;
+            altura = alt;
+        }
+        public void imprimeDados()
+        {
+            Console.WriteLine("Comprimento = " + comprimento);
+            Console.WriteLine("Altura = " + altura);
+
+        }
+
+        public void setComprimento( double comp)
+        {
+            comprimento = comp;
+
+        }
+        public void setAltura(double alt)
+        {
+            altura = alt;
+
+        }
+        public double getComprimento()
+        {
+            return comprimento;
+        }
+        public double getAltura()
+        {
+            return altura;
+        }
+
+        public double Area()
+        {
+            return (comprimento * altura);
+        }
+
+        public double Perimetro()
+        {
+            return ((comprimento + altura)*2);
+        }
+    }// fim da classe Retangulo
+}
